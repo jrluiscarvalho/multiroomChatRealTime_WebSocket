7637da40c9ba3049f8e729a95273428fa701d82d
Junior <lsco@gft.com>
2016-12-19 17:55:06 -0200

    socket io

--- app.js
+++ app.js
@@ -2,6 +2,33 @@
 var app = require('./config/server');
 
 /*parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor online');
+});
+
+var io = require('socket.io').listen(server);
+app.set('io', io);
+
+/*  criar conexao por websocket */
+io.on('connection', function(socket){
+    console.log('usuario conectou!');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    });
+
+    socket.on('msgParaServidor', function(data){
+        socket.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+
+
+    });
+
 });
