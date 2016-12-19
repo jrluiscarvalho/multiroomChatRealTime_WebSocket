de572f34b38c1a59a697533bb3b6365a9115c199
Junior <lsco@gft.com>
2016-12-19 15:04:16 -0200

    configuracoes inicias de routes e controllers

---
+++ config/server.js
@@ -0,0 +1,34 @@
+/* importar o modulo do express */
+var express = require('express');
+/* importar o modulo do consign */
+var consign = require('consign');
+/* importar o modulo do body-parser */
+var bodyParser = require('body-parser');
+/* importar o modulo do express-validator */
+var expressValidator = require('express-validator');
+
+/* iniciar o objeto do express */
+var app = express();
+
+/* setar as variaveis 'view-engine' e 'views' do express */
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+/*configuar o middleware express.static */
+app.use(express.static('./app/public'));
+
+/*configurar o middleware body-parser */
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*configurar o middleware express-validator */
+app.use(expressValidator());
+
+/* efetua o autoload das rotas, dos controllers e dos models */
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+
+/* exportar o objeto app*/
+module.exports = app;
